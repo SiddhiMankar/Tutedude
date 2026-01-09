037966a3392b774a97fa11b5ddca90295a6468a4
unknown <mankarsiddhi0403@gmail.com>
2026-01-09 22:37:02 +0530

    Assignment 2

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+#Task 1: Arithmetic Operations
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+#Task 2: Personalised Greeting
+fname = input("Enter your first name: ")
+lname = input("Enter your last name: ")
+print(f"Hello, {fname} {lname}! Welcome to python programme!")
